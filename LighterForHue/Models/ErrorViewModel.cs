779427b00d41777f6736e3ffd2a5dd7ab44b4c38
PathoProgrammer <g_pierzchala@outlook.com>
2022-01-25 09:31:56 +0100

    removed old project

--- LighterForHue/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LighterForHue.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
